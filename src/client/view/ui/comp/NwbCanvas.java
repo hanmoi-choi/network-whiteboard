a2e500db2c1343b827794599f49af3c821885973
Jungmin Son <fogony@gmail.com>
2012-10-20 04:03:03 +1100

    implement double buffer for Canvas object in order to prevent flickering

--- src/client/view/ui/comp/NwbCanvas.java
+++ src/client/view/ui/comp/NwbCanvas.java
@@ -91,4 +91,27 @@ public class NwbCanvas extends Canvas {
             strategyMap.get(shapeType).drawShape(g2D);
         }
     }
+    
+	// Implement Double Buffer to prevent fliker when draw a shape
+    @Override
+    public void update(Graphics g) {
+	Graphics offgc;
+	Image offscreen = null;
+	Dimension d = size();
+
+	// create the offscreen buffer and associated Graphics
+	offscreen = createImage(d.width, d.height);
+	offgc = offscreen.getGraphics();
+	
+	// clear the exposed area
+	offgc.setColor(getBackground());
+	offgc.fillRect(0, 0, d.width, d.height);
+	offgc.setColor(getForeground());
+	
+	// do normal redraw
+	paint(offgc);
+	
+	// transfer offscreen to window
+	g.drawImage(offscreen, 0, 0, this);
+    }
 }
